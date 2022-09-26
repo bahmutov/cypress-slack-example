494a822b53b07de4f459edd87b8692562987cc7e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-09-26 19:09:22 -0400

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# cypress-slack-example ![cypress version](https://img.shields.io/badge/cypress-10.8.0-brightgreen)
+
+> Notify specific people in listed channels on Slack when a test fails
---
+++ cypress.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  e2e: {
+    video: false,
+    env: {
+      grepFilterSpecs: true,
+      grepOmitFiltered: true,
+    },
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://localhost:8888',
+    specPattern: 'cypress/e2e/**/*spec.{js,ts}',
+  },
+})
