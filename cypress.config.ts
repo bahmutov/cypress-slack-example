f9f8075c4e6233980a2f2bc5c076bcc3236b2b22
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-09-26 19:13:54 -0400

    remove plugins folder and file

--- cypress.config.ts
+++ cypress.config.ts
@@ -11,7 +11,18 @@ export default defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      // https://github.com/cypress-io/cypress-grep
+      require('cypress-grep/src/plugin')(on, config)
+
+      on('task', {
+        getNumber() {
+          return 42
+        },
+      })
+
+      // make sure to return the config object
+      // as it might have been modified by the plugin
+      return config
     },
     baseUrl: 'http://localhost:8888',
     specPattern: 'cypress/e2e/**/*spec.{js,ts}',
